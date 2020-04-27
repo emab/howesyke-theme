64eee432c37850786d9965bff3b779935eaf7fef
Eddy Brown <eddybrown@me.com>
2020-04-27 13:27:44 +0100

    Update header

--- header.php
+++ header.php
@@ -18,8 +18,8 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/odometer.css" />"
 	<?php wp_head(); ?>
+	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/odometer.css" />"
 </head>
 
 <body <?php body_class(); ?> <?php understrap_body_attributes(); ?>>
