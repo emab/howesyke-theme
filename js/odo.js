e023ffb79adcb8f5ddf53b9eb86ec1a2655b731e
Eddy Brown <eddybrown@me.com>
2020-04-27 14:20:08 +0100

    Add odometer files

---
+++ js/odo.js
@@ -0,0 +1,6 @@
+setTimeout(function(){
+    odometer1.innerHTML = document.getElementById('tree-number').value;
+    odometer2.innerHTML = document.getElementById('hedge-number').value;
+    odometer3.innerHTML = document.getElementById('moorland-number').value;
+    odometer4.innerHTML = document.getElementById('electric-number').value;
+}, 1000);
